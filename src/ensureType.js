28ab688f2b07274737aefd64b1211cfa37727c75
Eduardo Velasques <eduveks@gmail.com>
2024-04-19 09:39:14 -0300

    Ensure types with support as required or optional.

--- src/ensureType.js
+++ src/ensureType.js
@@ -2,19 +2,35 @@ import { isArray, isObject, ensureObject } from "@xso/utils";
 import Component from "./Component";
 import jsonDefinition from "./jsonDefinition";
 
-function ensureSameKind(any, com) {
+function ensureType(any, com) {
+    const invalidError = (obj) => new Error(`Only ${com.function().name} type is accepted! This component is invalid: ${jsonDefinition(obj)}`);
     if (isArray(any)) {
         for (const obj of any) {
             ensureObject('ensureSameKind of an invalid object.', obj);
             if (!Component.isSameKind(obj, com)) {
-                throw new Error(`Only ${com.function().name} type is accepted! This component is invalid: ${jsonDefinition(obj)}`);
+                throw invalidError(obj);
             }
         }
     } else if (isObject(any)) {
-        return Component.isSameKind(any, com);
+        if (!Component.isSameKind(any, com)) {
+            throw invalidError(any);
+        }
     } else {
         throw new Error('Invalid kind.');
     }
 }
 
-export default ensureSameKind;
+ensureType.required = (any, com)=> {
+    if (any) {
+        return ensureType(any, com);
+    }
+    throw new Error(`${com.name()} is required.`);
+}
+
+ensureType.optional = (any, com)=> {
+    if (any) {
+        ensureType(any, com);
+    }
+}
+
+export default ensureType;
