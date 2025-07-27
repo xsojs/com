6280d1ce1d20f922abfcb51ed9f92d276d0e6022
Eduardo Velasques <eduveks@gmail.com>
2025-07-27 21:48:01 +0100

    Load Tag Props integrated with SVG class and XMLNS propagation.

--- src/loadTagProps.js
+++ src/loadTagProps.js
@@ -5,33 +5,33 @@ import {
 } from "@xso/utils";
 import render from "./render";
 
-function loadTagProps(tag, props, com) {
+function loadTagProps(tag, props, com, xmlns) {
     ensureObject('Properties', props);
     for (const key of Object.keys(props)) {
         const value = props[key];
-        if (key == '_' && value) {
+        if (key === '_' && value) {
             if (isArray(value)) {
-                render(tag, value, com);
+                render(tag, value, com, false, xmlns);
             } else if (isObject(value)) {
-                render(tag, [ value ], com);
+                render(tag, [ value ], com, false, xmlns);
             } else {
                 tag.textContent = value;
             }
-        } else if (key == '$' && value) {
+        } else if (key === '$' && value) {
             if (isArray(value)) {
-                render(tag, value, com, true);
+                render(tag, value, com, true, xmlns);
             } else if (isObject(value)) {
-                render(tag, [ value ], com, true);
+                render(tag, [ value ], com, true, xmlns);
             } else {
                 tag.innerHTML = value;
             }
-        } else if (key == 'text' && value) {
+        } else if (key === 'text' && value) {
             ensureString(key, value);
             tag.innerText = value;
-        } else if (key == 'html' && value) {
+        } else if (key === 'html' && value) {
             ensureString(key, value);
             tag.innerHTML = value;
-        } else if (key == 'style' && value) {
+        } else if (key === 'style' && value) {
             ensureObject(key, value);
             for (const styleKey of Object.keys(value)) {
                 if (styleKey.indexOf('--') === 0) {
@@ -40,34 +40,46 @@ function loadTagProps(tag, props, com) {
                     tag.style[styleKey] = value[styleKey];
                 }
             }
-        } else if (key == 'className' && value) {
+        } else if (key === 'className' && value) {
             ensureString(key, value);
-            tag.className = value;
-        } else if (key == 'classList' && value) {
-            loadClassList(tag, value);
-        } else if (key == 'class' && value) {
-            if (isString(value)) {
+            if (tag.className instanceof SVGAnimatedString) {
+                tag.className.baseVal = value;
+            } else {
                 tag.className = value;
+            }
+        } else if (key === 'classList' && value) {
+            if (tag.className instanceof SVGAnimatedString) {
+                tag.className.baseVal = value.join(' ');
+            } else {
+                loadClassList(key, tag, value);
+            }
+        } else if (key === 'class' && value) {
+            if (isString(value)) {
+                if (tag.className instanceof SVGAnimatedString) {
+                    tag.className.baseVal = value;
+                } else {
+                    tag.className = value;
+                }
             } else if (isArray(value)) {
-                loadClassList(tag, value);
-            } else if (value != undefined && value != null) {
+                loadClassList(key, tag, value);
+            } else if (value !== undefined && value !== null) {
                 throw new Error(`CSS Class of type ${typeof value} is not valid, only string or array of strings is accepted.`);
             }
-        } else if (key.indexOf('on') == 0) {
+        } else if (key.indexOf('on') === 0) {
             if (value) {
                 ensureFunction(key, value);
                 tag.addEventListener(key.substring(2).toLowerCase(), value);
             }
-        } else if (value != undefined && value != null) {
+        } else if (value !== undefined && value !== null) {
             tag.setAttribute(key, value);
         }
     }
 }
 
-function loadClassList(tag, prop) {
+function loadClassList(key, tag, prop) {
     ensureArray(key, prop);
     for (const cssClass of prop) {
-        ensureString(cssClass);
+        ensureString(key, cssClass);
         tag.classList.add(cssClass);
     }
 }
